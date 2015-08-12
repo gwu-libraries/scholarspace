8ec492afc1aee5b985e9a1fc999374b3ab13108f
kilahimm <matt.mihalik@gmail.com>
2015-08-12 15:27:14 -0400

    Update ability.rb

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,24 +3,6 @@ class Ability
   include Sufia::Ability
 
 
-  def sufia_abilities
-    generic_file_abilities
-    user_abilities
-    featured_work_abilities
-    editor_abilities
-    stats_abilities
-    citation_abilities
-    proxy_deposit_abilities
-  end
-
-  def stats_abilities
-    alias_action :stats, to: :read
-  end
-
-  def citation_abilities
-    alias_action :citation, to: :read
-  end
-  
   # Define any customized permissions here.
   def custom_permissions
     # Limits deleting objects to a the admin user
