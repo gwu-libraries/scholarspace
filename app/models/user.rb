293a316d66acc54727e75e6bea8686215675d95d
Matthew Mihalik <matt.mihalik@gmail.com>
2016-07-13 09:07:36 -0400

    Move block out of if/else/end statement

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,18 +13,19 @@ class User < ActiveRecord::Base
   end
   # Connects this user object to Blacklights Bookmarks. 
   include Blacklight::User
+
+  # Method added by Blacklight; Blacklight uses #to_s on your
+  # user class to get a user-displayable login/identifier for
+  # the account.
+  def to_s
+    email
+  end
   
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   if Rails.application.config.shibboleth == true
     devise :database_authenticatable, :trackable, :omniauthable, :omniauth_providers => [:shibboleth]
     
-    # Method added by Blacklight; Blacklight uses #to_s on your
-    # user class to get a user-displayable login/identifier for
-    # the account.
-    def to_s
-      email
-    end
     def self.from_omniauth(auth)
       user = find_by(provider: auth.provider, uid: auth.uid) || new(uid: auth.uid, provider: auth.provider)
       user.update!(email: auth.info.email,
@@ -39,10 +40,6 @@ class User < ActiveRecord::Base
   else
     devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
-    def to_s
-      email
-    end
   end
 
   def contentadmin?
