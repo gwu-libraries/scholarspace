c0544b1770ca70990b1c519e11dfc603c80dec00
Matt Mihalik <matt.mihalik@gmail.com>
2017-02-06 14:22:50 -0500

    Work around per:
    https://github.com/projecthydra/sufia/issues/1527

---
+++ app/controllers/downloads_controller.rb
@@ -0,0 +1,22 @@
+class DownloadsController < ApplicationController
+  include Sufia::DownloadsControllerBehavior
+
+  #FIXME: This is cached in sufia... means that users can't see files changed sometimes...
+  def file
+    #load_file
+    @file ||= load_file
+  end
+
+  def send_content
+    if file.mime_type == "application/pdf"
+      self.status = 200
+      response.headers['Content-Length'] = file.size.to_s
+      response.headers['Content-Disposition'] = "inline;filename=#{asset.label.gsub(/[,;]/, '')}.pdf"
+      render body: file.content, content_type: "application/pdf"
+    else
+      super
+    end
+
+  end
+
+end
